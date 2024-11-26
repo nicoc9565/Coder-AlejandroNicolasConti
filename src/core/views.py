2f6043e06fc4e3f21ef85965dc4119dfe042bfa2
Nicolas Conti <nicolasc9565@gmail.com>
2024-11-26 10:39:46 -0300

    agregando cositas de la clase

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def saludar(request):
+    return HttpResponse("Hola desde Django!")
+
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f"{apellido}, {nombre}")
+
+
+def index(request):
+    context = {"año": 2024}
+    return render(request, "core/index.html", context)
