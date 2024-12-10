7df10be1fad175e1c1985c0897bae77d1aa19fc2
Nicolas Conti <nicolasc9565@gmail.com>
2024-12-10 11:23:36 -0300

    Creo que ahora si funciona, proximo paso es darle detalle con algo de css a las paginas

--- src/gimnasio/urls.py
+++ src/gimnasio/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import index, about, AlumnoCreateView
+from .views import (
+    index,
+    about,
+    AlumnoCreateView,
+    AlumnoDetailView,
+    AlumnoUpdateView,
+    AlumnoDeleteView,
+)
 
 from . import views
 
@@ -11,4 +18,9 @@ urlpatterns = [
     path("about/", about, name="about"),
     path("alumnos/nuevo/", AlumnoCreateView.as_view(), name="alumno-create"),
     path("alumnos/", views.lista_alumnos, name="alumno-list"),
+    path("alumnos/<int:pk>/", AlumnoDetailView.as_view(), name="alumno-detail"),
+    path("alumnos/<int:pk>/editar/", AlumnoUpdateView.as_view(), name="alumno-edit"),
+    path(
+        "alumnos/<int:pk>/eliminar/", AlumnoDeleteView.as_view(), name="alumno-delete"
+    ),
 ]
--- src/gimnasio/views.py
+++ src/gimnasio/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
 from .models import Alumno, DiaAsistencia
 
@@ -17,15 +18,48 @@ class AlumnoCreateView(CreateView):
 
     def form_valid(self, form):
         alumno = form.save()
+        dias_asistencia = self.request.POST.getlist("dias_asistencia")
+        for dia_id in dias_asistencia:
+            dia_obj = DiaAsistencia.objects.get(id=dia_id)
+            alumno.dias_asistencia.add(dia_obj)
+        alumno.save()
+        return super().form_valid(form)
+
+
+class AlumnoDetailView(DetailView):
+    model = Alumno
+    template_name = "gimnasio/alumno_detail.html"
+    context_object_name = "alumno"
+
+
+class AlumnoUpdateView(UpdateView):
+    model = Alumno
+    fields = ["nombre", "edad", "altura", "horario", "cuota_pagada"]
+    template_name = "gimnasio/alumno_form.html"
+    success_url = reverse_lazy("gimnasio:alumno-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dias"] = DiaAsistencia.objects.all()
+        return context
+
+    def form_valid(self, form):
+        alumno = form.save()
 
         dias_asistencia = self.request.POST.getlist("dias_asistencia")
+        alumno.dias_asistencia.clear()
         for dia_id in dias_asistencia:
             dia_obj = DiaAsistencia.objects.get(id=dia_id)
             alumno.dias_asistencia.add(dia_obj)
 
         alumno.save()
-
         return super().form_valid(form)
+
+
+class AlumnoDeleteView(DeleteView):
+    model = Alumno
+    template_name = "gimnasio/alumno_confirm_delete.html"
+    success_url = reverse_lazy("gimnasio:alumno-list")
 
 
 def lista_alumnos(request):
